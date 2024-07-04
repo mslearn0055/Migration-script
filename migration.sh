ddc1250f025f2824410771824a3369377927c9eb
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-07-04 22:29:28 +0530

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -1,22 +1,22 @@
-#migration script
-
 #!/bin/bash 
 
 sourceRepo="$1"
 targetRepo="$2"
 
-source_repo_name=$(basename "$sourceRepo .git)
+# Fixing the syntax for extracting the source repo name
+source_repo_name=$(basename "$sourceRepo" .git)
 
-git clone --mirror "sourceRepo" wandisco_$source_repo_name
+# Fixing the condition and syntax errors in the if statement
+git clone --mirror "$sourceRepo" wandisco_$source_repo_name
 
-if { $? -ne 0 }: then 
-echo "source repo not found"
-exit 1
+if [ $? -ne 0 ]; then 
+  echo "source repo not found"
+  exit 1
 else 
-cd wandisco_$source_repo_name
-git remote add origin "$targetRepo"
-git push neworigin --all
-git push neworigin --tags 
-cd ..
-rm -rf wandisco_$source_repo_name
+  cd wandisco_$source_repo_name
+  git remote add origin "$targetRepo"
+  git push origin --all
+  git push origin --tags 
+  cd ..
+  rm -rf wandisco_$source_repo_name
 fi
