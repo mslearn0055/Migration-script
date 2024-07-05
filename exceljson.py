3d229049f390844bc36967763c043bcfa633e18d
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-07-05 14:33:09 +0530

    Create exceljson.py

---
+++ exceljson.py
@@ -0,0 +1,14 @@
+import xlrd
+import pandas as pd
+import json
+import sys
+
+def fileconvertion(filename):
+    df = pd.read_excel(filename, sheet_name='Sheet1')
+    json_data = df.to_json(orient='records')
+    json_data_pretty = json.dumps(json.loads(json_data), indent=4)
+    
+    with open('migration.json', 'w') as f:
+        f.write(json_data_pretty)
+
+fileconvertion(sys.argv[1])
